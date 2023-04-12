6871d309fce3ceccdbb8886512f7094d49dc05d7
willgpaik <bgh0919@gmail.com>
2023-04-12 02:11:39 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# ESP32-CAM program with Arduino IDE
+
+- This version let ESP32-CAM to run AP (Access Point) mode
+- Once powered, use portable device with WIFI enabled to connect
+  - Initial SSID: testAP-ardu
+  - Initial Password: 123456789
---
+++ esp32_camera.ino
@@ -0,0 +1,141 @@
+/*********  
+  Board setting:
+    AI-Thinker: ESP32-CAM with mother board
+    Wrover Module: ESP32-CAM connected to Arduino
+*********/
+
+
+// Load Wi-Fi library
+#include "esp_camera.h"
+#include <WiFi.h>
+
+// Camera model for ESP32 CAM:
+#define CAMERA_MODEL_AI_THINKER  // For ESP32-cam board
+//#define CAMERA_MODEL_WROVER_KIT // For ESP32 WROVER(-E)
+
+#include "camera_pins.h"
+
+// Replace with your network credentials
+const char* ssid = "testAP-ardu";
+const char* password = "123456789";
+void startCameraServer();
+void setupLedFlash(int pin);
+
+// Set web server port number to 80
+WiFiServer server(80);
+
+// Variable to store the HTTP request
+String header;
+
+// Auxiliar variables to store the current output state
+String output26State = "off";
+String output27State = "off";
+
+// Assign output variables to GPIO pins
+const int output26 = 26;
+const int output27 = 27;
+
+void setup() {
+  Serial.begin(115200);
+
+
+  // Connect to Wi-Fi network with SSID and password
+  Serial.print("Setting AP (Access Point)…");
+  // Remove the password parameter, if you want the AP (Access Point) to be open
+  WiFi.softAP(ssid, password);
+
+  IPAddress IP = WiFi.softAPIP();
+  Serial.print("AP IP address: ");
+  Serial.println(IP);
+
+  server.begin();
+
+
+  Serial.setDebugOutput(true);
+  Serial.println();
+
+  // Camera setup
+  camera_config_t config;
+  config.ledc_channel = LEDC_CHANNEL_0;
+  config.ledc_timer = LEDC_TIMER_0;
+  config.pin_d0 = Y2_GPIO_NUM;
+  config.pin_d1 = Y3_GPIO_NUM;
+  config.pin_d2 = Y4_GPIO_NUM;
+  config.pin_d3 = Y5_GPIO_NUM;
+  config.pin_d4 = Y6_GPIO_NUM;
+  config.pin_d5 = Y7_GPIO_NUM;
+  config.pin_d6 = Y8_GPIO_NUM;
+  config.pin_d7 = Y9_GPIO_NUM;
+  config.pin_xclk = XCLK_GPIO_NUM;
+  config.pin_pclk = PCLK_GPIO_NUM;
+  config.pin_vsync = VSYNC_GPIO_NUM;
+  config.pin_href = HREF_GPIO_NUM;
+  config.pin_sscb_sda = SIOD_GPIO_NUM;
+  config.pin_sscb_scl = SIOC_GPIO_NUM;
+  config.pin_pwdn = PWDN_GPIO_NUM;
+  config.pin_reset = RESET_GPIO_NUM;
+  config.xclk_freq_hz = 20000000;
+  config.frame_size = FRAMESIZE_SVGA; // 800*600 @ 30 fps
+  //config.frame_size = FRAMESIZE_UXGA; // max size @ 15fps
+  config.pixel_format = PIXFORMAT_JPEG;  // for streaming (original)
+  //config.pixel_format = PIXFORMAT_RGB565; // for face detection/recognition
+  config.grab_mode = CAMERA_GRAB_WHEN_EMPTY;
+  config.fb_location = CAMERA_FB_IN_PSRAM;
+  config.jpeg_quality = 12;
+  config.fb_count = 1;
+
+  // if PSRAM IC present, init with UXGA resolution (1600x1200) and higher JPEG quality
+  //                      for larger pre-allocated frame buffer.
+  if (config.pixel_format == PIXFORMAT_JPEG) {
+    if (psramFound()) {
+      config.jpeg_quality = 10;
+      config.fb_count = 2;
+      config.grab_mode = CAMERA_GRAB_LATEST;
+    } else {
+      // Limit the frame size when PSRAM is not available
+      config.frame_size = FRAMESIZE_SVGA; // 800x600
+      config.fb_location = CAMERA_FB_IN_DRAM;
+    }
+  } else {
+    // Best option for face detection/recognition
+    config.frame_size = FRAMESIZE_240X240;
+    #if CONFIG_IDF_TARGET_ESP32S3
+        config.fb_count = 2;
+    #endif
+  }
+
+  // camera init
+  esp_err_t err = esp_camera_init(&config);
+  if (err != ESP_OK) {
+    Serial.printf("Camera init failed with error 0x%x", err);
+    return;
+  }
+
+  sensor_t* s = esp_camera_sensor_get();
+  // initial sensors are flipped vertically and colors are a bit saturated
+  if (s->id.PID == OV3660_PID) {
+    s->set_vflip(s, 1);        // flip it back
+    s->set_brightness(s, 1);   // up the brightness just a bit
+    s->set_saturation(s, -2);  // lower the saturation
+  }
+  // drop down frame size for higher initial frame rate
+  if (config.pixel_format == PIXFORMAT_JPEG) {
+    s->set_framesize(s, FRAMESIZE_SVGA); // QVGA -> change to SVGA (320x240->800x600)
+  }
+
+  startCameraServer();
+
+  Serial.print("Camera Ready! Use 'http://");
+  Serial.print(IP);
+  Serial.println("' to connect");
+      
+  
+  // Setup LED FLash if LED pin is defined in camera_pins.h
+  #if defined(LED_GPIO_NUM)
+    setupLedFlash(LED_GPIO_NUM);
+  #endif
+}
+
+void loop() {
+
+}
